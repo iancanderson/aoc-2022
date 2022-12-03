de3a3c46ebc525bd983ab473417ba36afde3e860
Ian C. Anderson <ian@iancanderson.com>
2022-12-03 06:52:26 -0500

    Day 2

---
+++ day02-pt1.rb
@@ -0,0 +1,79 @@
+class Round
+  def initialize(opponent_shape_code:, my_shape_code:)
+    @opponent_shape_code = opponent_shape_code
+    @my_shape_code = my_shape_code
+  end
+
+  def my_score
+    score = 0
+
+    if i_won?
+      score += 6
+    elsif draw?
+      score += 3
+    end
+
+    case my_shape
+    when :rock then score += 1
+    when :paper then score += 2
+    when :scissors then score += 3
+    end
+  end
+
+  private
+
+  def i_won?
+    game_result == :won
+  end
+
+  def draw?
+    game_result == :draw
+  end
+
+  def game_result
+    @game_result ||= begin
+      if my_shape == opponent_shape
+        :draw
+      else
+        case my_shape
+        when :rock then opponent_shape == :scissors ? :won : :lost
+        when :paper then opponent_shape == :rock ? :won : :lost
+        when :scissors then opponent_shape == :paper ? :won : :lost
+        end
+      end
+    end
+  end
+
+  def my_shape
+    case @my_shape_code
+    when "X"
+      :rock
+    when "Y"
+      :paper
+    when "Z"
+      :scissors
+    else
+      raise "Unknown shape code: #{@my_shape_code}"
+    end
+  end
+
+  def opponent_shape
+    case @opponent_shape_code
+    when "A"
+      :rock
+    when "B"
+      :paper
+    when "C"
+      :scissors
+    else
+      raise "Unknown shape code: #{@opponent_shape_code}"
+    end
+  end
+end
+
+rounds = File.readlines("day02.txt").map do |line|
+  opponent_shape, my_shape = line.split(" ")
+  Round.new(opponent_shape_code: opponent_shape, my_shape_code: my_shape)
+end
+
+puts rounds.sum(&:my_score)
