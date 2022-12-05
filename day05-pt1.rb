d16f142a1be7ffd178b4ac00e6ba4ef60069fc77
Ian C. Anderson <ian@iancanderson.com>
2022-12-05 08:24:31 -0500

    Day 5

---
+++ day05-pt1.rb
@@ -0,0 +1,67 @@
+require "active_support/all"
+require "awesome_print"
+
+class Stack
+  def initialize
+    @ary = []
+  end
+
+  def push(el)
+    @ary.push(el)
+  end
+
+  def pop
+    @ary.pop
+  end
+
+  def peek
+    @ary.last
+  end
+end
+
+Instruction = Struct.new(:amount, :from, :to, keyword_init: true)
+INSTRUCTION_REGEX = /move (?<amount>\d+) from (?<from>\d+) to (?<to>\d+)/
+
+crates_top_down = []
+instructions = []
+
+File.readlines("day05.txt").each do |line|
+  if line.include? "["
+    # break up line into 4-character chunks
+    row = line.chars.each_slice(4).map do |chunk|
+      chunk[1].presence
+    end
+    crates_top_down << row
+  elsif line.include? "move"
+    INSTRUCTION_REGEX.match(line) do |m|
+      instructions << Instruction.new(
+        amount: m[:amount].to_i,
+        from: m[:from].to_i - 1, # zero-index
+        to: m[:to].to_i - 1,     # zero-index
+      )
+    end
+  end
+end
+
+stack_count = crates_top_down.map(&:length).max
+stacks = Array.new(stack_count) { Stack.new }
+
+# Push crates onto stacks from bottom to top
+crates_top_down.reverse.each do |row|
+  row.each_with_index do |crate, index|
+    stacks[index].push(crate) if crate
+  end
+end
+
+
+instructions.each do |instruction|
+  from_stack = stacks[instruction.from]
+  to_stack = stacks[instruction.to]
+
+  instruction.amount.times do
+    crate = from_stack.pop
+    to_stack.push(crate)
+  end
+end
+
+puts stacks.map(&:peek).join
