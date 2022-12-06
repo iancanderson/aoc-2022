81e9ee90cff86506ad43db6ad173d597607de575
Ian C. Anderson <ian@iancanderson.com>
2022-12-06 07:32:03 -0500

    Remove unused var

--- day06.rb
+++ day06.rb
@@ -1,7 +1,5 @@
 input_chars = File.read("day06.txt").chars
 
-part1_chunk_size = 4
-
 def chars_until_unique_chunk(input_chars, chunk_size)
   result = chunk_size - 1
   input_chars.each_cons(chunk_size).detect do |chunk|
