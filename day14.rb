ab196f24449bf7b54090fa205b0ac5b090738e2c
Ian C. Anderson <ian@iancanderson.com>
2022-12-15 08:16:03 -0500

    Day 14 pt 2

--- day14.rb
+++ day14.rb
@@ -31,6 +31,8 @@ Path = Struct.new(:ends) do
 end
 
 class Cave
+  attr_reader :depth
+
   def initialize
     @stones = {}
     @sands = {}
@@ -101,7 +103,7 @@ paths.each do |path|
 end
 
 def spawn_sand
-  puts "Spawned a new unit of sand"
+  # puts "Spawned a new unit of sand"
   Point.new(500, 0)
 end
 
@@ -109,8 +111,6 @@ falling_sand = spawn_sand
 units_of_sand_at_rest = 0
 
 loop do
-  # puts cave.to_s
-
   if falling_sand.nil?
     falling_sand = spawn_sand
   end
@@ -120,7 +120,6 @@ loop do
     break
   end
 
-  puts "Moving sand"
   if cave.air?(falling_sand.x, falling_sand.y + 1)
     # move down
     falling_sand = Point.new(falling_sand.x, falling_sand.y + 1)
@@ -139,3 +138,48 @@ loop do
 end
 
 puts "Part 1: #{units_of_sand_at_rest}"
+
+cave = Cave.new
+paths.each do |path|
+  cave.add_stone_path(path)
+end
+
+# Add floor for part 2
+cave.add_stone_path(Path.new([
+  Point.new(-1_000, cave.depth + 2),
+  Point.new(1_000, cave.depth + 2),
+]))
+
+falling_sand = spawn_sand
+units_of_sand_at_rest = 0
+spawn_point = Point.new(500, 0)
+
+loop do
+  # puts cave.to_s
+
+  if falling_sand.nil?
+    falling_sand = spawn_sand
+  end
+
+  if cave.air?(falling_sand.x, falling_sand.y + 1)
+    # move down
+    falling_sand = Point.new(falling_sand.x, falling_sand.y + 1)
+  elsif cave.air?(falling_sand.x - 1, falling_sand.y + 1)
+    # move down and left
+    falling_sand = Point.new(falling_sand.x - 1, falling_sand.y + 1)
+  elsif cave.air?(falling_sand.x + 1, falling_sand.y + 1)
+    # move down and right
+    falling_sand = Point.new(falling_sand.x + 1, falling_sand.y + 1)
+  else
+    # sand rests: add it to the cave
+    units_of_sand_at_rest += 1
+    cave.add_sand(falling_sand)
+    if !cave.air?(spawn_point.x, spawn_point.y)
+      puts "Sand entrance is blocked"
+      break
+    end
+    falling_sand = nil
+  end
+end
+
+puts "Part 2: #{units_of_sand_at_rest}"
